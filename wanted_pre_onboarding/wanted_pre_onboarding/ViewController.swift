85e3802b9acf3a77852edfbbfc0161e0e4eec22a
SChan-Kim <instufld@gmail.com>
2022-09-03 21:10:54 +0900

    feat: initial commit

---
+++ wanted_pre_onboarding/wanted_pre_onboarding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wanted_pre_onboarding
+//
+//  Created by 김승찬 on 2022/09/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
